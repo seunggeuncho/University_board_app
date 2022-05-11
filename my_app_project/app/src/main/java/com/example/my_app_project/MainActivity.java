126c6fea32f5cda82e3025755bbebde1e97e1502
unknown <wkfyddl1tp@naver.com>
2022-05-11 17:28:56 +0900

    modify

--- my_app_project/app/src/main/java/com/example/my_app_project/MainActivity.java
+++ my_app_project/app/src/main/java/com/example/my_app_project/MainActivity.java
@@ -6,11 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
@@ -21,37 +24,52 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
     private SignInButton btn_google; //구글 로그인 버튼
     private FirebaseAuth auth; //파이어 베이스 인증 객체
-    private GoogleApiClient googleApiClient; //googleApiClient;
+    private GoogleSignInClient mGoogleSignInClient; //googleApiClient;
     private static final int REQ_SIGN_GOOGLE = 100; //구글 로그인 결과 코드
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        auth = FirebaseAuth.getInstance();
+       /*if (auth.getCurrentUser() != null){
+            Intent intent = new Intent(getApplication(), choose.class);
+            intent.putExtra("nickName", String.valueOf(auth.getCurrentUser().getDisplayName()));
+            intent.putExtra("photoUrl", String.valueOf(auth.getCurrentUser().getPhotoUrl()));
+            startActivity(intent);
+            finish();
+        }*/
+        Log.v("Current User--------------",String.valueOf(auth.getCurrentUser()));
         GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(com.firebase.ui.auth.R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
-        googleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this,this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
-                .build();
-        auth = FirebaseAuth.getInstance(); //파이어베이스 인증 객체 초기화.
+
+        mGoogleSignInClient= GoogleSignIn.getClient(this, googleSignInOptions);
+
 
         btn_google = findViewById(R.id.btn_google);
         btn_google.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                Intent intent = mGoogleSignInClient.getSignInIntent();
                 startActivityForResult(intent,REQ_SIGN_GOOGLE);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = auth.getCurrentUser();
     }
 
     @Override
@@ -61,6 +79,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             if(result.isSuccess()){
                 GoogleSignInAccount account = result.getSignInAccount();
+                Log.v("accountId",account.getId());
                 resultLogin(account);
             }
         }
@@ -78,9 +97,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                          Intent intent = new Intent(getApplicationContext(), choose.class);
                          intent.putExtra("nickName", account.getDisplayName());
                          intent.putExtra("photoUrl", String.valueOf(account.getPhotoUrl()));
-
-
                          startActivity(intent);
+                         finish();
                      } else{
                          Toast.makeText(MainActivity.this, "로그인 실패", Toast.LENGTH_LONG).show();
                      }
--- my_app_project/app/src/main/java/com/example/my_app_project/choose.java
+++ my_app_project/app/src/main/java/com/example/my_app_project/choose.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,11 +12,15 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.io.File;
 
 public class choose extends AppCompatActivity {
     private TextView tv_result;
     private ImageView iv_profile;
-    private GoogleApiClient mActivity;
+    private GoogleApiClient mGoogleApiClient;
+    private FirebaseAuth mAuth;
     String nickName;
     String photoUrl;
 
@@ -30,6 +35,7 @@ public class choose extends AppCompatActivity {
 
         tv_result = findViewById(R.id.textView);
         tv_result.setText(nickName);
+        mAuth = FirebaseAuth.getInstance();
 
         iv_profile = findViewById(R.id.iv_profile);
         Glide.with(this).load(photoUrl).into(iv_profile);
@@ -40,18 +46,16 @@ public class choose extends AppCompatActivity {
     }
 
 
-        private void signOut() {
-            if (mActivity.isConnected()) {
-                // Google sign out
-                Auth.GoogleSignInApi.signOut(mActivity);
-            }
-        }
+    private void signOut() {
+        mAuth.signOut();
+    }
 
 
         View.OnClickListener onClickListener = (v) -> {
             switch (v.getId()) {
                 case R.id.btn_back:
                     signOut();
+                    myStartActivity(MainActivity.class);
                     break;
                 case R.id.question_btn:
                     myStartActivity(Question_board.class);
