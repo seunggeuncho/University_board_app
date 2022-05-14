65d0f314e1507b44249c2f79e62bc97fd326b9e6
unknown <wkfyddl1tp@naver.com>
2022-05-14 16:04:21 +0900

    modify

--- my_app_project/app/src/main/java/com/example/my_app_project/MainActivity.java
+++ my_app_project/app/src/main/java/com/example/my_app_project/MainActivity.java
@@ -39,13 +39,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         setContentView(R.layout.activity_main);
 
         auth = FirebaseAuth.getInstance();
-       /*if (auth.getCurrentUser() != null){
-            Intent intent = new Intent(getApplication(), choose.class);
-            intent.putExtra("nickName", String.valueOf(auth.getCurrentUser().getDisplayName()));
-            intent.putExtra("photoUrl", String.valueOf(auth.getCurrentUser().getPhotoUrl()));
-            startActivity(intent);
-            finish();
-        }*/
+       
         GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(com.firebase.ui.auth.R.string.default_web_client_id))
                 .requestEmail()
